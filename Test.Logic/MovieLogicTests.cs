6dbe5c1b5ea13f930358142d46c086a8d94b1957
tkov.dev <tommy.pk17@gmail.com>
2024-09-29 09:11:57 +0200

    add test example

---
+++ Test.Logic/MovieLogicTests.cs
@@ -0,0 +1,43 @@
+using Data.Access;
+using Domain.Access.Interfaces;
+using Domain.Entities.Movie;
+using Logic;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using FluentAssertions;
+
+namespace Test.Logic;
+
+[TestClass]
+public class MovieLogicTests
+{
+    [TestMethod]
+    public void CheckTicket_WhenValidTicket_ReturnTrue()
+    {
+        var mockMovieTicket = new Mock<MovieTicket>();
+        var mockMovie = new Mock<Movie>();
+        var mockEntityFactory = new Mock<IEntityFactory>();
+        
+        mockMovieTicket.Object.ShowingTime = DateTime.Now.AddMinutes(100);
+        mockMovie.Object.RunTime = 100;
+
+        mockMovieTicket.Setup(x => x.Get(It.IsAny<int>()))
+            .Returns(mockMovieTicket.Object);
+        
+        mockMovie.Setup(x => x.Get(It.IsAny<int>()))
+            .Returns(mockMovie.Object);
+        
+        mockEntityFactory.Setup(x => x.Instantiate<MovieTicket>())
+            .Returns(mockMovieTicket.Object);
+        mockEntityFactory.Setup(x => x.Instantiate<Movie>())
+            .Returns(mockMovie.Object);
+
+        mockMovieTicket.Setup(x => x.Update()).Returns(mockMovieTicket.Object);
+        
+        var movieLogic = new MovieLogic(mockEntityFactory.Object);
+
+        var actual = movieLogic.CheckTicket(1);
+
+        actual.Should().BeTrue();
+    }
+}
